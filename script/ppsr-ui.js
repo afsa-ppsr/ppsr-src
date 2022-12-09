e49124a8e22a4bab82251cd62028baa492cc80fc
offerj <jamesoffer@googlemail.com>
2022-12-09 10:46:15 +0800

    .

--- script/ppsr-ui.js
+++ script/ppsr-ui.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
-  alert('f');
+  $("#wrap").append("<div id='mobile-menu-list'>"+$("#menu").html()+"</div>");
+
+  $(".menuNav").append("<button id='mobile-menu'>Menu</button>");
+
+  $("#mobile-menu").bind( "click", function() {
+    $("#mobile-menu-list").css("display","block");
+    //$("#mobile-menu-list").css("opacity",0);
+
+    $("#mobile-menu-list").width($(window).width());
+
+
+    var offset = $(this).offset();
+    var heightOffset = offset.top + ($(".menuNav").height());
+
+
+    $("#mobile-menu-list").css("top", heightOffset+"px");
+    $("#mobile-menu-list").height($(window).height()-heightOffset);
+
+  });
+
 
 });
