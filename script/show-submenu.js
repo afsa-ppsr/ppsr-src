1e4e4d6927abfd0c71084b175411ef766b37a30e
greengypsy <31978630+greengypsy@users.noreply.github.com>
2023-01-03 11:01:57 +1030

    Update show-submenu.js

--- script/show-submenu.js
+++ script/show-submenu.js
@@ -3,7 +3,7 @@ function showSubMenu(){
 
   var submenu = submenugroup[0];
   
-  if (submenu.style.display = "none"){
+  if (submenu.style.display === "none"){
     submenu.style.display = "inline-block";
   } else {
     submenu.style.display = "none";
