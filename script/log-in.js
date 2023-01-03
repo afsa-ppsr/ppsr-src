1713e1e03ad08a956234eb9fde2c745b70c516ef
greengypsy <31978630+greengypsy@users.noreply.github.com>
2023-01-03 14:51:52 +1030

    Create log-in.js

---
+++ script/log-in.js
@@ -0,0 +1,8 @@
+function login(){
+ var username = document.getElementById("username");
+ var password = document.getElementById("password");
+  
+ if (username.value == "TestAccount" and password.value == "ppsraccount"){
+   window.location.href = "https://afsa-ppsr.github.io/ppsr/home.html";
+ }
+}
