e0fb91a451b30ef3d02e75b8fe40b43b3c912294
greengypsy <ckumar073@gmail.com>
2023-01-13 15:12:44 +1030

    modify fill-form.js

--- script/fill-form.js
+++ script/fill-form.js
@@ -1,9 +1,10 @@
-document.addEventListener('readystatechange', e => {
+document.addEventListener('change', e => {
     var field = document.getElementById("account-customer-name");
 
     if (field != null){ 
         var field_value = field.value;
 
-        sessionStorage.setItem('account_customer_name', field.value);
+        sessionStorage.setItem('account_customer_name', field_value);
     }
 });
+
